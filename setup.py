5863726180823947a369fdaf76c3b4883bb8c60f
H. Chase Stevens <chase@chasestevens.com>
2017-02-22 20:50:23 +0000

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='monkeys',
     packages=['monkeys', 'monkeys.tools'],
-    version='0.0.8',
+    version='0.0.9',
     description='A strongly-typed genetic programming framework',
     license='MIT',
     author='H. Chase Stevens',
